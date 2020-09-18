49389c69911c60a07a8872eaa8b18d7a070c534e
Moises Andrade <lawliex@gmail.com>
2020-09-19 02:41:13 +1200

    added product model

--- InventoryManager/product/models.py
+++ InventoryManager/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from InventoryManager.category.models import Category
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, verbose_name = 'Category')
+    product_name = models.CharField('Product', max_length = 255)
+    product_description = models.TextField(null = True)
+    product_quantity = models.IntegerField(default = 0)
+    product_image = models.ImageField(upload_to = 'product/')
+
+    def __str__(self):
+        return self.product_name
